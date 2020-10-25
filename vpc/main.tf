2008fcfddffcb9dc8563a624a2b9e71660b7569c
kalvin <kalvin.vasconcellos@gmail.com>
2020-10-25 17:26:34 -0300

    chore: terraform fmt vpc

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,11 +1,10 @@
-
 resource "aws_vpc" "vpc" {
   cidr_block           = "${var.cidr}"
   enable_dns_hostnames = true
 
   tags = {
-    Name        =  "${var.name}"
-    Environment =  "${var.environment}"
+    Name        = "${var.name}"
+    Environment = "${var.environment}"
   }
 }
 
@@ -13,6 +12,6 @@ resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = "${aws_vpc.vpc.id}"
 
   tags = {
-    Environment =  "${var.environment}"
+    Environment = "${var.environment}"
   }
 }
